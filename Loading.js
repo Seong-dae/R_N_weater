3de10b0ecd55092a7230b876afb49bb22d0c6147
Seong-dae <42955124+Seong-dae@users.noreply.github.com>
2020-02-13 17:00:57 +0900

    Using expo-location library

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading(){
+    return (
+    <View style = {styles.container}>
+        <Text style = {styles.text}>Getting the Weather Info</Text>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: 'flex-end',
+        // padding: 20,
+        paddingHorizontal: 30,
+        backgroundColor: "#FDF6AA",
+    },
+    text: {
+        color: "#2c2c2c",
+        fontSize: 50,
+    }
+})
